6536a66cf4ee2523bb9774745cc9913e9492fb08
Philip Lemaster <philiplemaster@yahoo.com>
2020-03-05 23:12:35 -0500

    Finished N64 button and API pull

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
-// Adding event listener to cat button
-$("#genButton").on("click", function() {
+// N64 BUTTON API PULL AND DOM CREATION
 
-    // Getting API url for cat images
+$("#n64Button").on("click", function() {
+
     var queryURL = "https://api-v3.igdb.com/games";
 
-    // Ajax code to grab images from 'queryURL'
     $.ajax({
       url: queryURL,
       method: "POST",
@@ -13,18 +12,45 @@ $("#genButton").on("click", function() {
           'Accept' : 'Application/JSON'
       }, 
 
-      data : 'fields name,summary; where platforms = 4 ; sort popularity desc; limit 50;'
-        
-               
+      data : 'fields name,rating,cover.url,release_dates.human,platforms.name; where platforms = 4 ; sort popularity desc; limit 50;'
+      })
 
-            
-    })
-
-    // Run a function after 'GETting' from Ajax
       .then(function(response) {
+        var results = response;
+        coverimage = response.cover
+        console.log(response);
 
-      console.log(response);
+        var table = $('#results');
+        var tBody = $('<tbody>');
+        
+
+        for (var i = 0; i < 50; i++) {
+            var tRow = $('<tr>');
+            tRow.appendTo(tBody);
+            tBody.appendTo(table);
+
+            var title = $('<th>').text(results[i].name);
+            title.appendTo(tRow);
+
+            source = String(results[i].cover?.url);
+            var image = $('<img>');
+            image.attr("src", "https://" + source);
+            image.appendTo(tRow);
+
+            var rating = $('<th>').text(Math.round(parseInt(results[i].rating)));
+            rating.appendTo(tRow);
+
+            var releaseDates = $('<th>').text(results[i].release_dates[0]?.human);
+            releaseDates.appendTo(tRow);
+
+            var moreContent = $('<button>More Info</button>');
+            $(moreContent).addClass('clear button warning');
+            moreContent.appendTo(tRow);
+            
+          }
       });
+
+      
   });
 
   jQuery.ajaxPrefilter(function(options) {
